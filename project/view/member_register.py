a71fb8e10ea29eb15bd4d169160a607009455a5e
YWT <theabc50111@gmail.com>
2021-12-30 14:36:39 +0800

    add main.py & member_register and view folder and add blueprint to main.py

---
+++ project/view/member_register.py
@@ -0,0 +1,22 @@
+from flask import render_template, request, redirect, url_for, Blueprint
+import sqlalchemy as db
+from sqlalchemy import func
+from datetime import datetime
+import math
+
+
+
+member_app = Blueprint('member_app', __name__)
+
+@member_app.route('/')
+def index():
+    return render_template('index.html',
+                           page_header="member register",
+                           current_time=datetime.utcnow())
+
+
+@member_app.route('/form')
+def member_data_edit():
+    return render_template('index.html',
+                           page_header="member data edit",
+                           current_time=datetime.utcnow())

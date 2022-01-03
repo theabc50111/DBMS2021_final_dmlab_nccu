3a5d5aef103d06f27227424f52e42eb30367cb65
evaneversaydie <eva608770@gmail.com>
2022-01-03 16:56:07 +0800

    add database into Scrapping

--- project/view/property_scrap.py
+++ project/view/property_scrap.py
@@ -8,9 +8,19 @@ import math
 
 pp_scrap_app = Blueprint('property_scrap_app', __name__, url_prefix="/pp_scrap")
 
+# sql setting
+path_to_db = "./db/LabPropertyMgt20211231.db"
+table_ScrappingInfo = 'ScrappingInfo'
+table_ScrappingList = 'ScrappingList'
+engine = db.create_engine(f'sqlite:///{path_to_db}')
+metadata = db.MetaData()
+table_customers = db.Table(table, metadata, autoload=True, autoload_with=engine)
+
+
 @pp_scrap_app.route('/')
 def index():
     return render_template('index.html',
                            page_header="property scrap",
                            current_time=datetime.utcnow())
+
 

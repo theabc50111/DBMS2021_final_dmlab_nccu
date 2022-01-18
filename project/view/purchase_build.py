cace201816a6514286c6ea1bafd19f7c4eaa5f3e
YUNG-RU JENG <79561973+YungRuJeng@users.noreply.github.com>
2022-01-18 12:29:26 +0800

    Update purchasing function

--- project/view/purchase_build.py
+++ project/view/purchase_build.py
@@ -3,12 +3,6 @@ import sqlalchemy as db
 from sqlalchemy import func
 from datetime import datetime
 import math
-import pandas as pd
-
-
-# sql setting
-path_to_db = "./db/LabPropertyMgt20211231.db"
-
 from sqlalchemy.engine import Engine
 from sqlalchemy import event
 
@@ -17,64 +11,117 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor = dbapi_connection.cursor()
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()
-
-
-
-
-engine = db.create_engine(f'sqlite:///{path_to_db}',native_datetime=True)
-metadata = db.MetaData()
-table_PurchasingInfo = db.Table('PurchasingInfo', metadata, autoload=True, autoload_with=engine)
-table_PurchasingList = db.Table('PurchasingList', metadata, autoload=True, autoload_with=engine)
-table_Member = db.Table('Member', metadata, autoload=True, autoload_with=engine)
-table_Supplier = db.Table('Supplier', metadata, autoload=True, autoload_with=engine)
-
+    
 pc_build_app = Blueprint('purchase_build_app', __name__, url_prefix="/pc_build")
 
+path_to_db = "./db/LabPropertyMgt20211231.db"
+table_name_PurchasingInfo = 'PurchasingInfo'
+table_name_PurchasingList = 'PurchasingList'
+# table_name_Distribution = 'Distribution'
+# table_name_BargainingQuotation = 'BargainingQuotation'
+engine = db.create_engine(f'sqlite:///{path_to_db}',native_datetime=True)
+
+
+metadata = db.MetaData()
+table_PurchasingInfo = db.Table(table_name_PurchasingInfo, metadata, autoload=True, autoload_with=engine)
+table_PurchasingList  = db.Table(table_name_PurchasingList, metadata, autoload=True, autoload_with=engine)
+
+
 @pc_build_app.route('/')
-def index():
-    return render_template('index.html',
-                           page_header="purchase build",
-                           current_time=datetime.utcnow())
+def index(): #show PurchasingList
+
+    page = int(request.args.get('page') if request.args.get('page') else 1)
+    each_page = 30
+
+    connection  = engine.connect() 
+    query = db.select(func.count()).select_from(table_PurchasingInfo)
+    proxy = connection.execute(query)
+    total_pages = math.ceil(proxy.fetchall()[0][0]/each_page)
+
+    query = db.select(table_PurchasingInfo).limit(each_page).offset((page-1)*each_page)
+    proxy = connection.execute(query)
+    results = proxy.fetchall()
+    print(results[0].keys())
 
 
-@pc_build_app.route('/Purchasing_submit', methods=["GET", "POST"])
+    connection.close()
+    
+    return render_template('PurchasingList_table.html',
+                           page_header="所有申請之採購物品清單",
+                           total_pages=total_pages,
+                           outputs=results,
+                           page=page)
+
+
+@pc_build_app.route('/pclist_submit', methods=["GET", "POST"])
 def Purchasing_submit():
-    List_ID_GENERATE="None"
+    PurchasingList_SN_GENERATE="None"
+
     if request.method=="POST":
         try:
-            connection  = engine.connect() # connection 要放在view function中，否則會出現thread error
-            query = db.select(table_PurchasingList).order_by(table_PurchasingList.c.List_ID)
+            connection  = engine.connect() 
+            query = db.select(table_PurchasingList.c.List_ID).order_by(table_PurchasingList.c.List_ID)
             proxy = connection.execute(query)
-            id_list = [idx[0] for idx in proxy.fetchall()]  
+            id_list = [idx[0] for idx in proxy.fetchall()]
 
-            if request.form['Sn']&request.form['Item']&request.form['Specification']&request.form['Amount']: # 希望至少要填寫名子
+            # query = db.select(table_PurchasingList.c.SN).order_by(table_PurchasingList.c.SN)
+            # proxy = connection.execute(query)
+            # Item_Sn_list = [idx[0] for idx in proxy.fetchall()]
+  
+            # query = db.select(table_Distribution.c.PurchasedItem_Sn).order_by(table_PurchasingList.c.PurchasedItem_Sn)
+            # proxy = connection.execute(query)
+            # Item_Sn_list_distr = [idx[0] for idx in proxy.fetchall()]
+  
+            # query = db.select(table_BargainingQuotation.c.Item_Sn).order_by(table_PurchasingList.c.Item_Sn)
+            # proxy = connection.execute(query)
+            # Item_Sn_list_bq = [idx[0] for idx in proxy.fetchall()]
+  
+
+            if request.form['Item']&request.form['Specification']&request.form['Amount']: 
                 query = db.select(table_PurchasingList.c.List_ID).select_from(table_PurchasingList).order_by(table_PurchasingList.c.List_ID.desc())
                 proxy = connection.execute(query)
-                List_ID_GENERATE = 'P'+str(int([idx[0] for idx in proxy.fetchall()][0].split('P')[1])+1)        
+                PurchasingList_SN_GENERATE = 'S'+str(int([idx[0] for idx in proxy.fetchall()][0].split('S')[1])+1)
 
-                query = db.insert(PurchasingInfo).values(Applicant_ID=request.form['Applicant_ID'],List_ID=List_ID_GENERATE,SubmitDate=request.form['Date'],Purchaser_ID=None,AcceptDate=None, TransactionDate=None, DeliveryDate=None, Supplier_ID=None)
+                query = db.insert(table_PurchasingList).values(SN=PurchasingList_SN_GENERATE,Item=request.form['Item'],Specification=request.form['Specification'],Amount=request.form['Amount'])
                 proxy = connection.execute(query)
-
-                query = db.insert(table_PurchasingList).values(List_ID=List_ID_GENERATE,Sn=request.form['Sn'],Item=request.form['Item'],Specification=request.form['Specification'], Amount=request.form['Amount'])
-                proxy = connection.execute(query)                
-
+                # query = db.insert(table_Distribution).values(PurchasedItem_Sn=PurchasingList_SN_GENERATE)
+                # proxy = connection.execute(query)
+                # query = db.insert(table_BargainingQuotation).values(Item_Sn=PurchasingList_SN_GENERATE)
+                # proxy = connection.execute(query)
             else:
                 raise Exception
         except:
-            return render_template('purchasing_submit.html',
-                                    page_header="提交採購清單")
-        
+            return render_template('purchasingList_submit.html',
+                                    page_header="建立採購物品清單",id_list=id_list,status="Failed",
+                                    PurchasingList_SN_GENERATE=PurchasingList_SN_GENERATE)
+                                    #Item_Sn_list=Item_Sn_list,Item_Sn_list_distr=Item_Sn_list_distr,Item_Sn_list_bq=Item_Sn_list_bq,
+        else:
+            return render_template('purchasingList_submit.html',
+                                    page_header="建立採購物品清單",id_list=id_list,status="Success",
+                                    PurchasingList_SN_GENERATE=PurchasingList_SN_GENERATE)
+                                    #Item_Sn_list=Item_Sn_list,Item_Sn_list_distr=Item_Sn_list_distr,Item_Sn_list_bq=Item_Sn_list_bq,
         finally:
-            # Close connection
             connection.close()
            
     if request.method=="GET":
-        connection  = engine.connect() # connection 要放在view function中，否則會出現thread error
+        connection  = engine.connect() 
         query = db.select(table_PurchasingList.c.List_ID).order_by(table_PurchasingList.c.List_ID)
         proxy = connection.execute(query)
         id_list = [idx[0] for idx in proxy.fetchall()]
+        # query = db.select(table_PurchasingList.c.SN).order_by(table_PurchasingList.c.SN)
+        # proxy = connection.execute(query)
+        # Item_Sn_list = [idx[0] for idx in proxy.fetchall()]
+  
+        # query = db.select(table_Distribution.c.PurchasedItem_Sn).order_by(table_PurchasingList.c.PurchasedItem_Sn)
+        # proxy = connection.execute(query)
+        # Item_Sn_list_distr = [idx[0] for idx in proxy.fetchall()]
+  
+        # query = db.select(table_BargainingQuotation.c.Item_Sn).order_by(table_PurchasingList.c.Item_Sn)
+        # proxy = connection.execute(query)
+        # Item_Sn_list_bq = [idx[0] for idx in proxy.fetchall()]
+
         connection.close()
 
-        return render_template('purchasing_submit.html',
-                                page_header="提交採購清單",id_list=id_list,id_list_property=id_list_property)
+        return render_template('purchasingList_submit.html',
+                                    page_header="建立採購物品清單",id_list=id_list)
 
